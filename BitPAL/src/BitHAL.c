fe051e9cab591bedf9187f62c9acfd392a336127
danwalmsley <dan@walms.co.uk>
2014-01-29 23:42:19 +0100

    showing platform initialosation

--- BitPAL/src/BitHAL.c
+++ BitPAL/src/BitHAL.c
@@ -8,19 +8,40 @@
  ============================================================================
  */
 
+#define PLATFORM_WINCE	0x8000
+#define PLATFORM_BITTHUNDER	0x8001
+#define PLATFORM_STM32F4DISCOVERY 0x8002;
 
+#define PLATFORM	PLATFORM_WINCE
 
+#if PLATFORM	== PLATFORM_WINCE
 #include "WinCEPlatform.h"
+#elif PLATFORM	== PLATFORM_BITTHUNDER
+#include "BitThunderPlatform.h"
+#elif PLATFORM 	== PLATFORM_STM32F4DISCOVERY
+#include "STM32F4DiscoveryPlatform.h"
+#endif
 
 IPlatform* Platform;
-byte DataBuffer[100];
+
+IPlatform* InitialisePlatform()
+{
+#if PLATFORM	== PLATFORM_WINCE
+	return new_WinCEPlatform();
+#elif PLATFORM	== PLATFORM_BITTHUNDER
+	return new_BitThunderPlatform();
+#elif PLATFORM 	== PLATFORM_STM32F4DISCOVERY
+	return new_STM32F4DiscoveryPlatform();
+#endif
+}
 
 int main(void)
 {
+	byte DataBuffer[100];
 	int bytesReceived = 0;
 	int bytesSent = 0;
 
-	Platform = new_WinCEPlatform();
+	Platform = InitialisePlatform();
 
 	Platform->Network->Start();
 
